47c2d077a48a0df46ff443eea60bb854f97f2568
vikas-15 <63404492+vikas-15@users.noreply.github.com>
2022-04-27 13:15:08 +0530

    Add files via upload

---
+++ test_wallet.py
@@ -0,0 +1,36 @@
+import pytest
+from Wallet import Wallet, InsufficientAmount
+
+
+def test_default_initial_amount():
+    wallet = Wallet()
+    assert wallet.balance == 0
+
+def test_setting_initial_amount():
+    wallet = Wallet(100)
+    assert wallet.balance == 100
+
+def test_wallet_add_cash():
+    wallet = Wallet(10)
+    wallet.add_cash(90)
+    assert wallet.balance == 100
+
+def test_wallet_spend_cash():
+    wallet = Wallet(20)
+    wallet.spend_cash(10)
+    assert wallet.balance == 10
+
+def test_wallet_spend_cash_raises_exception_on_insufficient_amount():
+    wallet = Wallet()
+    with pytest.raises(InsufficientAmount):
+        wallet.spend_cash(100)
+
+@pytest.mark.parametrize("earned,spent,expected", [
+    (30, 10, 20),
+    (20, 2, 18),
+])
+def test_transactions(earned, spent, expected):
+    my_wallet = Wallet()
+    my_wallet.add_cash(earned)
+    my_wallet.spend_cash(spent)
+    assert my_wallet.balance == expected
