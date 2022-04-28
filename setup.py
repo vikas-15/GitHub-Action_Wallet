6c401f1d9c34f7e9977d3505dff02b8de2188f1d
vikas-15 <63404492+vikas-15@users.noreply.github.com>
2022-04-28 13:44:07 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Wallet",
+    version="0.0.5",
+    author="Vikas Bhardwaj",
+    author_email="viaksbhardwaj020@gmail.com",
+    url="https://drive.google.com/file/d/123aztyIl9KSax3OkF6h_Rv6KlcCHkxo6/view?usp=sharing",
+    description="An aplication that helps to maintain wallet balance",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["click", "pytz"],
+    entry_points={"console_scripts": ["Wallet = Wallet"]},
+)
